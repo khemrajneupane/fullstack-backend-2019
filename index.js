439d250702b5da393fb4200a5d2bafff9ed72fcf
NeupaneKhemRaj <shekharneupane@gmail.com>
2019-07-29 21:47:30 +0300

    routing improved with all services, yet not much of error handlings

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
+var jsonfile = require("jsonfile");
+
+const filePath = __dirname + "/" + "persons.json";
+
 const cors = require("cors");
 app.use(cors());
 
@@ -19,84 +23,136 @@ morgan.token("data", function(req, res) {
 
 app.use(morgan(":method :url :status :response-time ms :data"));
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
-  },
-  {
-    name: "ABC",
-    number: "39-23-6423122",
-    id: 5
-  }
-];
-app.get("/api/persons", (req, res) => {
-  res.send(persons);
+/**http://localhost:3001/api/persons */
+app.get("/api/persons/", function(req, res) {
+  jsonfile
+    .readFile(filePath)
+    .then(obj => {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(JSON.stringify(obj));
+    })
+    .catch(() => {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end();
+      console.error("Reading server side JSON file failed.");
+    });
 });
 
+/**http://localhost:3001/info */
 app.get("/info", (req, res) => {
   let date = new Date();
-  res.end(`Phonebook has info for ${persons.length} people
+  jsonfile
+    .readFile(filePath)
+    .then(obj => {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(`Phonebook has info for ${obj.length} people
 ${date}`);
+    })
+    .catch(error => {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end(error);
+    });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
+/**http://localhost:3001/persons/1 */
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
 
-  const person = persons.find(person => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end("resource not found, 404");
-  }
+  jsonfile
+    .readFile(filePath)
+    .then(obj => {
+      res.send(obj.find(person => person.id === id));
+    })
+
+    .catch(error => {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end(error);
+    });
+});
+/**http://localhost:3001/persons/1 */
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  jsonfile
+    .readFile(filePath)
+    .then(obj => {
+      jsonfile.writeFile(filePath, obj.filter(person => person.id !== id));
+      res.send(obj.filter(person => person.id !== id));
+    })
+    .catch(error => {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end(error);
+    });
 });
 
-app.delete("/persons/remove/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.filter(person => person.id !== id);
-
-  response.end(`${person[0].name} has been deleted `);
-});
-
-app.post("/api/persons", (request, response) => {
+/**http://localhost:3001/api/persons/*/
+app.post("/api/persons/", (req, res) => {
   const generateId = () => {
     const thiId = Math.floor(Math.random() * 500);
     return thiId;
   };
-  const body = request.body;
+
+  const body = req.body;
   const person = {
     name: body.name,
-    number: body.number,
+    number: Number(body.number),
     id: generateId()
   };
-  if (!person.name || !person.number) {
-    return response.json({
-      error: "either or both of name and numbers missing"
-    });
-  }
+  if (person.name && person.number) {
+    jsonfile
+      .readFile(filePath)
+      .then(obj => {
+        const nameArr = obj.map(n => n.name === person.name);
 
-  const nameArray = persons.map(n => n.name);
-  nameArray.indexOf(person.name) > -1
-    ? response.json({
-        error: "name must be unique"
+        if (nameArr.indexOf(true) < 0) {
+          jsonfile.writeFile(filePath, obj.concat(person));
+          res.send(obj.concat(person));
+        } else {
+          console.log("this name is already used");
+          jsonfile.writeFile(filePath, obj);
+          res.send(obj);
+        }
       })
-    : response.send(persons);
+      .catch(error => {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        console.dir(error);
+        res.end();
+      });
+  } else {
+    res.end();
+    console.log("name number errors");
+  }
+});
+/**put */
+app.put("/api/persons/:id", (req, res) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: Number(body.number),
+    id: body.id
+  };
+  jsonfile
+    .readFile(filePath)
+    .then(obj => {
+      const newArr = obj.filter(m => m.id !== person.id);
+      console.log(newArr);
+      jsonfile.writeFile(filePath, newArr.concat(person));
+      res.send(newArr.concat(person));
+      /*  const namesArray = obj.filter(n => n.name !== person.name);
+      const nArray = obj.filter(n => n.name !== person.name);
+      //const numArray = obj.map(n => n.id === person.id);
+      console.log(nArray);
+      const matchingNames = nArray.filter(n => n.name === person.name);
+      console.log(matchingNames);
+      jsonfile.writeFile(filePath, namesArray.concat(person));
+      res.send(namesArray.concat(person));
+      */
+    })
+    .catch(error => {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      console.dir(error);
+      res.end();
+    });
 });
 
 const PORT = process.env.PORT || 3001;
