ce016c7eda2a9cec0de192d828584addcb85d700
NeupaneKhemRaj <shekharneupane@gmail.com>
2019-08-11 14:03:14 +0300

    mongodb url corrected in the frontend

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4a9a4d97ecd51ba499f0052568b95163.js"
+  "/precache-manifest.f140f15ec6c388f2689f026017c02b97.js"
 );
 
 self.addEventListener('message', (event) => {
